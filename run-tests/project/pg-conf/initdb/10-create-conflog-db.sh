e71a482002db1863e4448ea034e236b6e90e8d80
Alex <whites.russia@gmail.com>
2026-03-02 17:30:18 +0300

    [fix]: создание таблицы в 2 этапа

--- run-tests/project/pg-conf/initdb/10-create-conflog-db.sh
+++ run-tests/project/pg-conf/initdb/10-create-conflog-db.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e
-# set -a; . .env.pxf; set +a
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER ${CONFLOG_USERNAME} WITH PASSWORD '${CONFLOG_PASSWORD}';
 	CREATE DATABASE ${CONFLOG_DATABASE};
 	GRANT ALL PRIVILEGES ON DATABASE ${CONFLOG_DATABASE} TO ${CONFLOG_USERNAME};
-	CREATE TABLE users (
+EOSQL
+
+PGPASSWORD="$CONFLOG_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$CONFLOG_USERNAME" --dbname "$CONFLOG_DATABASE" <<-EOSQL
+	CREATE SCHEMA IF NOT EXISTS test;
+	CREATE TABLE test.users (
 		id SERIAL PRIMARY KEY,
 		username VARCHAR(50) UNIQUE NOT NULL,
 		email VARCHAR(100) UNIQUE,
 		age INTEGER CHECK (age >= 0),
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);
-	INSERT INTO users (username, email, age)
+	INSERT INTO test.users (username, email, age)
 	VALUES 
 	('cyber_punk', 'punk@neon.city', 20),
 	('tech_wizard', 'wizard@code.io', 32),
