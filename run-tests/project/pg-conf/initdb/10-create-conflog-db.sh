8a2d06e1f66900747579eedf6c2effd929d82016
Alex <whites.russia@gmail.com>
2026-03-02 16:59:20 +0300

    [ref]: через переменные окружения + тестовая таблица

--- run-tests/project/pg-conf/initdb/10-create-conflog-db.sh
+++ run-tests/project/pg-conf/initdb/10-create-conflog-db.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -e
+# set -a; . .env.pxf; set +a
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER usr_prodlog01 WITH PASSWORD 'password111';
-	CREATE DATABASE test_log01;
-	GRANT ALL PRIVILEGES ON DATABASE test_log01 TO usr_prodlog01;
+	CREATE USER ${CONFLOG_USERNAME} WITH PASSWORD '${CONFLOG_PASSWORD}';
+	CREATE DATABASE ${CONFLOG_DATABASE};
+	GRANT ALL PRIVILEGES ON DATABASE ${CONFLOG_DATABASE} TO ${CONFLOG_USERNAME};
+	CREATE TABLE users (
+		id SERIAL PRIMARY KEY,
+		username VARCHAR(50) UNIQUE NOT NULL,
+		email VARCHAR(100) UNIQUE,
+		age INTEGER CHECK (age >= 0),
+		created_at TIMESTAMP DEFAULT CURRENT__TIMESTAMP
+	);
+	INSERT INTO users (username, email, age)
+	VALUES 
+	('cyber_punk', 'punk@neon.city', 20),
+	('tech_wizard', 'wizard@code.io', 32),
+	('sql_master', NULL, 45);
 EOSQL
