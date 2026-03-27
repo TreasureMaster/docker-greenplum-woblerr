004076b310c4d6822e77117dacbda5efccf08d7d
Alex <whites.russia@gmail.com>
2026-03-27 17:52:25 +0300

    [ref]: build jenkins без кеша

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -2,4 +2,5 @@
 
 IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg5"
 
-docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
+# docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
+docker build --progress=plain -t "$IMAGE_TAG" .
