fc73c55775a3dba181cc60c1505fa56aaff2424b
Alex <whites.russia@gmail.com>
2026-04-02 11:37:47 +0300

    [add]: docker в образ jenkins

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg6.2"
+IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.0"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
