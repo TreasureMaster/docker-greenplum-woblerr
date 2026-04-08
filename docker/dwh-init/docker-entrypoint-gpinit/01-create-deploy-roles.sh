f592c7d0c740756120d69a97b776b4fab76b2a12
Alex <whites.russia@gmail.com>
2026-04-08 17:16:53 +0300

    [fix]: grant

--- docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
+++ docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
@@ -6,7 +6,7 @@ GREENPLUM_PASSWORD=$(cat "${GP_PASSWORD_FILE}" | tr -d '\n')
 PGPASSWORD="$GREENPLUM_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$GREENPLUM_USER" --dbname "$GREENPLUM_DATABASE_NAME" <<-EOSQL
     CREATE ROLE adb_deploys WITH SUPERUSER CREATEROLE;
     CREATE ROLE adb_deploy_cdjks WITH PASSWORD '${ADB_DEPLOY_CDJKS_PASSWORD}';
-    GRANT ROLE adb_deploys TO adb_deploy_cdjks;
+    GRANT adb_deploys TO adb_deploy_cdjks;
 EOSQL
 
 unset "${GP_PASSWORD_FILE}"
