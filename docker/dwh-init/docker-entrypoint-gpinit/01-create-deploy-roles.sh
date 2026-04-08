3601f096ff64491f61f13c182bf99a0002d75d98
Alex <whites.russia@gmail.com>
2026-04-08 16:53:56 +0300

    [fix]: запоминаем путь к паролю gp

--- docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
+++ docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-GREENPLUM_PASSWORD=$(cat "${GREENPLUM_PASSWORD_FILE}" | tr -d '\n')
+GREENPLUM_PASSWORD=$(cat "${GP_PASSWORD_FILE}" | tr -d '\n')
 
 PGPASSWORD="$GREENPLUM_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$GREENPLUM_USER" --dbname "$GREENPLUM_DATABASE_NAME" <<-EOSQL
     CREATE ROLE adb_deploys WITH SUPERUSER CREATEROLE;
     CREATE ROLE adb_deploy_cdjks WITH PASSWORD '${ADB_DEPLOY_CDJKS_PASSWORD}';
     GRANT ROLE adb_deploys TO adb_deploy_cdjks;
 EOSQL
+
+unset "${GP_PASSWORD_FILE}"
--- docker/files/libenv.sh
+++ docker/files/libenv.sh
@@ -2,3 +2,6 @@
 
 # Common env vars
 gp_tmp_dir="/tmp/greenplum-cfg"
+if [ -d "${GREENPLUM_SECRETS_DIR}" ] && [ -n "$(ls -A "${GREENPLUM_SECRETS_DIR}" 2>/dev/null)" ]; then
+    GP_PASSWORD_FILE="${GREENPLUM_PASSWORD_FILE}"
+fi
