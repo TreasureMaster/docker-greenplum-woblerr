d22a0a159e77543a8d79934c7229b10e01646054
Alex <whites.russia@gmail.com>
2026-04-09 16:52:20 +0300

    [add]: роль adb_exec_dwh

--- docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
+++ docker/dwh-init/docker-entrypoint-gpinit/01-create-deploy-roles.sh
@@ -9,6 +9,8 @@ PGPASSWORD="$GREENPLUM_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$GREENPLUM_
     GRANT adb_deploys TO adb_deploy_cdjks;
     CREATE ROLE grp_admins;
     CREATE ROLE grp_supports;
+    CREATE ROLE adb_exec_dwh;
+    CREATE ROLE adb_exec_dwh_small WITH LOGIN PASSWORD '${ADB_EXEC_DWH_PASSWORD}';
 EOSQL
 
 unset GP_PASSWORD_FILE
