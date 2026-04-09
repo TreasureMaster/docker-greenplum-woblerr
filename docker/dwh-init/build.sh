69bb29586c19abca97e9a1a2724bcfff46e5cf16
Alex <whites.russia@gmail.com>
2026-04-09 11:24:14 +0300

    [add]: шаг работы с liquibase

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh-init:0.4.4"
+IMAGE_TAG="dwh-init:0.4.5"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
