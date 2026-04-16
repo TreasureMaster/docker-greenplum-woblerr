e20f4bd0d66ccfe434620cbcbb51a018132a903b
Alex <whites.russia@gmail.com>
2026-04-16 12:53:06 +0300

    [add]: refresh-mv

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.2"
+IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.3"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
