d0297c026ab9ca0955c3f35b249c6acfde3659e1
Alex <whites.russia@gmail.com>
2026-04-17 13:21:03 +0300

    [fix]: точка с запятой

--- docker/dwh-init/pg-conf/initdb/10-create-conflog-db.sh
+++ docker/dwh-init/pg-conf/initdb/10-create-conflog-db.sh
@@ -21,7 +21,7 @@ PGPASSWORD="$CONFLOG_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$CONFLOG_USER
 	CREATE ROLE rl_lm_worker;
 	CREATE ROLE rl_log_worker;
 	CREATE ROLE cdjks_prodlog_deploy WITH PASSWORD '${CDJKS_PRODLOG_DEPLOY_PASSWORD}';
-	CREATE ROLE cdjks_dumper WITH PASSWORD '${CDJKS_DUMPER_PASSWORD}'
+	CREATE ROLE cdjks_dumper WITH PASSWORD '${CDJKS_DUMPER_PASSWORD}';
 	GRANT CREATE ON DATABASE db_prod_log01 TO rl_owner_db;
 	GRANT CONNECT ON DATABASE db_prod_log01 TO rl_owner_db;
 	GRANT db_prod_log01 TO rl_owner_db;
