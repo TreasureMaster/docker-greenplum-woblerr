68f72b52ddcdf617dbda160e526439091522bd5b
Alex <whites.russia@gmail.com>
2026-04-24 16:39:45 +0300

    [fix]: airflow_elt в gitlab

--- docker/dwh-init/gitlabenv.sh
+++ docker/dwh-init/gitlabenv.sh
@@ -40,6 +40,7 @@ declare -A PROJECTS=(
     ["DWH/ADB/service-projects/automation/roles_ddl_repo.git"]=""
     ["DWH/ADB/service-projects/automation/jks-s2t-to-xlsx.git"]=""
     ["DWH/ADB/service-projects/automation/jks-automatic-generate-and-deploy-detail-dags.git"]=""
+    ["DWH/ADB/service-projects/airflow_elt.git"]=""
 )
 # Репозитории, где пользователи добавляются как Maintainer (40)
 MAINTAINER_PROJECTS=(
