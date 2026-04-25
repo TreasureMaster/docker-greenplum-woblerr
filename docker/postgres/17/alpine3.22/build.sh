50d0716d270b851429ad8ae0cdba1daaa235433a
Alex <whites.russia@gmail.com>
2026-04-25 12:47:22 +0300

    [fix]: заблокирован openpgp

--- docker/postgres/17/alpine3.22/build.sh
+++ docker/postgres/17/alpine3.22/build.sh
@@ -3,5 +3,5 @@
 IMAGE_TAG="dwh/postgres:17.9-alpine3.22-cs1"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
-# docker build --progress=plain -t "$IMAGE_TAG" .
-docker build -t "$IMAGE_TAG" .
+docker build --progress=plain -t "$IMAGE_TAG" .
+# docker build -t "$IMAGE_TAG" .
