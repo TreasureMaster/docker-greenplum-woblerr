bbb025ec05e51dba1aa9794ba3a9de929b990814
Alex <whites.russia@gmail.com>
2026-04-25 14:44:59 +0300

    [add]: пароль для cdjks_dumper

--- run-tests/postgres/preinitdb/20-create-users.sh
+++ run-tests/postgres/preinitdb/20-create-users.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -a; . /docker-entrypoint-initdb.d/.env; set +a
 
 echo "Running root initialization..."
 
 # Создание пользователя (идемпотентно)
-USERNAME="cdjks_dumper"
-HOME_DIR="/home/${USERNAME}"
+CDJKS_DUMPER_USER="cdjks_dumper"
+HOME_DIR="/home/${CDJKS_DUMPER_USER}"
 
-if ! id -u "$USERNAME" &>/dev/null; then
-    echo "Creating user: $USERNAME"
-    adduser -D -h "$HOME_DIR" -s /bin/bash "$USERNAME"
+if ! id -u "$CDJKS_DUMPER_USER" &>/dev/null; then
+    echo "Creating user: $CDJKS_DUMPER_USER"
+    adduser -D -h "$HOME_DIR" -s /bin/bash "$CDJKS_DUMPER_USER"
+    echo "$CDJKS_DUMPER_USER:$CDJKS_DUMPER_PASSWORD" | chpasswd
 
     # Пример: добавление в группу для доступа к общим директориям
-    # addgroup "$USERNAME" dockerhost
+    # addgroup "$CDJKS_DUMPER_USER" dockerhost
 
-    echo "User $USERNAME created with home $HOME_DIR"
+    echo "User $CDJKS_DUMPER_USER created with home $HOME_DIR"
 else
-    echo "User $USERNAME already exists, skipping."
+    echo "User $CDJKS_DUMPER_USER already exists, skipping."
 fi
 
 # Если нужно дать права на выполнение скриптов в конкретной директории:
