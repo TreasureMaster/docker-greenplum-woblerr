7286c8ae422e316e08695a261414cf81a6e33029
Alex <whites.russia@gmail.com>
2026-05-14 09:56:36 +0300

    [fix]: названия образов af при сборке

--- docker/airflow-worker/build.sh
+++ docker/airflow-worker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh/bitnami-airflow-worker:2.9.3-debian-12-r6-t11.ssh"
+IMAGE_TAG="dwh/bitnami-airflow-worker:2.9.3-debian12-r6-t11.ssh"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
