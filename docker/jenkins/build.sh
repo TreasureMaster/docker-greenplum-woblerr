6da160d4cfe0af77105b85a9329d8c982d8505f1
Alex <whites.russia@gmail.com>
2026-05-18 10:02:08 +0300

    [add]: deploy src dags

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.11"
+IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.12"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
