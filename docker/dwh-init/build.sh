57558192cab84e36c3b89b782cf0a97b91e4601c
Alex <whites.russia@gmail.com>
2026-05-29 10:29:15 +0300

    [ref]: bump dwh-init

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh-init:0.11.4"
+IMAGE_TAG="dwh-init:0.11.5"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
