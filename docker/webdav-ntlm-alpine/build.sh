1e8558f8fdc9e703498951f7451b069018ff4fcd
Alex <whites.russia@gmail.com>
2026-06-22 15:53:43 +0300

    [ref]: сборка с кешем

--- docker/webdav-ntlm-alpine/build.sh
+++ docker/webdav-ntlm-alpine/build.sh
@@ -2,4 +2,5 @@
 
 IMAGE_TAG="dwh/webdav-ntlm:0.0.5-alpine"
 
-docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
+# docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
+docker build --progress=plain -t "$IMAGE_TAG" .
