f0c6ea96102b0337a4a3d5ecc0fa9a1d0967dad8
Alex <whites.russia@gmail.com>
2026-06-23 17:27:43 +0300

    [fix]: папки в кавычках

--- docker/webdav-ntlm-alpine/entrypoint.sh
+++ docker/webdav-ntlm-alpine/entrypoint.sh
@@ -41,8 +41,8 @@ chown -R root:www-data /var/lib/samba/winbindd_privileged 2>/dev/null || true
 chmod 750 /var/lib/samba/winbindd_privileged 2>/dev/null || true
 
 # Создаем папки для s2t
-mkdir -p /var/www/webdav/_api/web/GetFolderByServerRelativeUrl('DocLib/S2T/Актуальные/S2T_RDV')/Files
-mkdir -p /var/www/webdav/_api/web/GetFolderByServerRelativeUrl('DocLib/S2T/Актуальные/S2T_STG')/Files
+mkdir -p "/var/www/webdav/_api/web/GetFolderByServerRelativeUrl('DocLib/S2T/Актуальные/S2T_RDV')/Files"
+mkdir -p "/var/www/webdav/_api/web/GetFolderByServerRelativeUrl('DocLib/S2T/Актуальные/S2T_STG')/Files"
 chown -R 82:82 /var/www/webdav
 chmod -R 775 /var/www/webdav
 
