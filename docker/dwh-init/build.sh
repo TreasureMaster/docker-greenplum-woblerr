59eeffa82703d15bdf76db7abb6b02b41368630e
Alex <whites.russia@gmail.com>
2026-06-23 16:22:46 +0300

    [ref]: запуск работы труб с файловойо шарой

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh-init:0.12.0"
+IMAGE_TAG="dwh-init:0.12.1"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
