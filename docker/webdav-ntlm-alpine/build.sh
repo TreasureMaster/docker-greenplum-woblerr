dab9e8b8bd5e32dde9c80dd246da11275492d423
Alex <whites.russia@gmail.com>
2026-06-25 18:03:55 +0300

    [add]: gdbm

--- docker/webdav-ntlm-alpine/build.sh
+++ docker/webdav-ntlm-alpine/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh/webdav-ntlm:0.0.10-alpine"
+IMAGE_TAG="dwh/webdav-ntlm:0.0.11-alpine"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
