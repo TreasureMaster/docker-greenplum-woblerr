bba116da9cf3949417cb836c185f763976f458db
Alex <whites.russia@gmail.com>
2026-06-29 11:39:53 +0300

    [ref]: bump ntlm-server version

--- docker/webdav-ntlm-alpine/build.sh
+++ docker/webdav-ntlm-alpine/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="dwh/webdav-ntlm:0.0.12-alpine"
+IMAGE_TAG="dwh/webdav-ntlm:0.1.0-alpine"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
