d52cd44ddc8c07ca7ba8dceb9a51641029a4472e
Alex <whites.russia@gmail.com>
2026-07-03 11:30:12 +0300

    [add]: суз в jenkins для настройки airflow коннектов

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.15"
+IMAGE_TAG="jenkins/jenkins:2.516.3-lts-plg7.16"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
