82a37848c6a17a4c81e7f0851916b539217c8d99
Alex <whites.russia@gmail.com>
2026-07-03 14:40:11 +0300

    [fix]: добавить метод getAt в разрешенные в jenkins

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG="treasuremaster/jenkins:2.516.3-lts-plg7.16"
+IMAGE_TAG="treasuremaster/jenkins:2.516.3-lts-plg7.17"
 
 # docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
 docker build --progress=plain -t "$IMAGE_TAG" .
