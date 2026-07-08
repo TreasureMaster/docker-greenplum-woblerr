fd200673027688cdc7fc68ba1cf79170bf4d3ff1
Alex <whites.russia@gmail.com>
2026-07-08 12:17:32 +0300

    [fix]: предупреждения в entrypoint greenplum

---
+++ docker/greenplum6-rebuild/files/entrypoint.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+# Load libraries
+. /liblog.sh
+. /libenv.sh
+
+uid=$(id -u)
+
+if [ "${uid}" = "0" ]; then
+    # Пользовательский часовой пояс.
+    if [ "${TZ}" != "Etc/UTC" ]; then
+        cp /usr/share/zoneinfo/${TZ} /etc/localtime
+        echo "${TZ}" > /etc/timezone
+    fi
+    # Пользовательская группа.
+    if [ "${GREENPLUM_GROUP}" != "gpadmin" ] || [ "${GREENPLUM_GID}" != "1001" ]; then
+        groupmod -g ${GREENPLUM_GID} -n ${GREENPLUM_GROUP} gpadmin
+    fi
+    # Настройка пользователя.
+    if [ "${GREENPLUM_USER}" != "gpadmin" ] || [ "${GREENPLUM_UID}" != "1001" ]; then
+        debug "Change user to ${GREENPLUM_USER} or uid to ${GREENPLUM_UID}"
+        java_home_path=$(dirname $(dirname $(readlink -f $(which java))))
+        usermod -g ${GREENPLUM_GID} -l ${GREENPLUM_USER} -u ${GREENPLUM_UID} -m -d /home/${GREENPLUM_USER} gpadmin
+        echo "source /usr/local/greenplum-db/greenplum_path.sh" > /home/${GREENPLUM_USER}/.bashrc
+        echo "export JAVA_HOME=/${java_home_path}" >> /home/${GREENPLUM_USER}/.bashrc
+        echo 'export PATH="/usr/local/pxf/bin:${PATH}"' >> /home/${GREENPLUM_USER}/.bashrc
+        # echo "export PXF_BASE=${GREENPLUM_DATA_DIRECTORY}/pxf" >> /home/${GREENPLUM_USER}/.bashrc
+        echo "export PXF_BASE=${GREENPLUM_PXF_BASE_DIRECTORY}" >> /home/${GREENPLUM_USER}/.bashrc
+        mkdir -m 700 -p /home/${GREENPLUM_USER}/.ssh
+        mkdir -p /home/${GREENPLUM_USER}/pxf
+        ssh-keygen -q -f /home/${GREENPLUM_USER}/.ssh/id_rsa -t rsa -N ""
+    fi
+    if [ "${GREENPLUM_PXF_BASE_DIRECTORY}" != "${GREENPLUM_DATA_DIRECTORY}/pxf" ]; then
+        debug "Change PXF_BASE path"
+        if [ -d "${GREENPLUM_DATA_DIRECTORY}/pxf" ]; then
+            debug "Delete old pxf dir"
+            rm -rf "${GREENPLUM_DATA_DIRECTORY}/pxf"
+        fi
+        mkdir -p ${GREENPLUM_PXF_BASE_DIRECTORY}
+        echo "export PXF_BASE=${GREENPLUM_PXF_BASE_DIRECTORY}" >> /home/${GREENPLUM_USER}/.bashrc
+    fi
+    debug "Create jenkins user (for ci/cd - ansible)"
+    if ! id "$JENKINS_SSH_USER" &>/dev/null; then
+        echo "Создаём пользователя: $JENKINS_SSH_USER"
+        useradd -m -s /bin/bash "$JENKINS_SSH_USER"
+
+        # Устанавливаем пароль
+        echo "$JENKINS_SSH_USER:$JENKINS_SSH_PASS" | chpasswd
+
+        # Опционально: добавляем в группу sudo (если нужно)
+        # usermod -aG sudo "$JENKINS_SSH_USER"
+    fi
+    # Проверяем, установлено ли правило
+    debug "Add jenkins user enter as gpadmin"
+    if [ ! -f "$SUDOERS_FILE" ] || ! grep -qF "$GPADMIN_RULE" "$SUDOERS_FILE"; then
+        echo "Настраиваем sudo-права для $JENKINS_SSH_USER → gpadmin"
+        echo "$GPADMIN_RULE" > "$SUDOERS_FILE"
+        chmod 440 "$SUDOERS_FILE"  # Обязательные права для sudoers
+        chown root:root "$SUDOERS_FILE"
+    fi
+    debug "Correction user:group"
+    # Коррекция user:group, если они были переопределены в env.
+    chown -R ${GREENPLUM_USER}:${GREENPLUM_GROUP} \
+        /home/${GREENPLUM_USER} \
+        ${GREENPLUM_DATA_DIRECTORY} \
+        ${GREENPLUM_PXF_BASE_DIRECTORY}
+        # /docker-entrypoint-initdb.d
+    # Коррекция user:group для стартовых файлов
+    chown ${GREENPLUM_USER}:${GREENPLUM_GROUP} \
+        /start_gpdb.sh \
+        /liblog.sh \
+        /libenv.sh
+    # Копирование проброшенных конфигурационных файлов, чтобы избежать изменения прав на хосте
+    mkdir -p ${gp_tmp_dir}
+    debug "Copy gpinitsystem config to local tmp"
+    if [ -f /tmp/gptempcfg/gpinitsystem_config ]; then
+        echo "INFO - Copy gpinitsystem_config to ${gp_tmp_dir}"
+        cp /tmp/gptempcfg/gpinitsystem_config "${gp_tmp_dir}"
+    else
+        debug "gpinitsystem_config not found in /tmp/gptempcfg"
+    fi
+    debug "Copy hostfile gpinitsystem to local tmp"
+    if [ -f /tmp/gptempcfg/hostfile_gpinitsystem ]; then
+        echo "INFO - Copy hostfile_gpinitsystem to ${gp_tmp_dir}"
+        cp /tmp/gptempcfg/hostfile_gpinitsystem "${gp_tmp_dir}"
+    else
+        debug "hostfile_gpinitsystem not found in /tmp/gptempcfg"
+    fi
+    debug "Copy pxf config to local tmp"
+    if [ -d /tmp/pxf ]; then
+        echo "INFO - Copy pxf config to ${gp_tmp_dir}"
+        cp -r /tmp/pxf "${gp_tmp_dir}"
+    fi
+    chown -R ${GREENPLUM_USER}:${GREENPLUM_GROUP} ${gp_tmp_dir}
+fi
+
+# Старт SSH сервера.
+debug "Start ssh server"
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    ssh-keygen -A
+fi
+mkdir -p /run/sshd
+/usr/sbin/sshd
+sleep 2
+
+# Выполнение команды.
+debug "Start start_gpdb.sh"
+if [ "${uid}" = "0" ]; then
+    exec gosu ${GREENPLUM_USER} "$@"
+else
+    exec "$@"
+fi
