9cbc8b52265969940723adc3ecca1ae653856688
Alex <whites.russia@gmail.com>
2026-07-13 17:44:58 +0300

    [add]: создание архива проекта после его сборки

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
-IMAGE_TAG="dwh-init:0.12.4"
+VERSION="0.12.4"
+IMAGE_TAG="dwh-init:${VERSION}"
 
-# docker build --no-cache --progress=plain -t "$IMAGE_TAG" .
+OUTPUT_DIR="$HOME/images"
+OUTPUT_FILE="${OUTPUT_DIR}/dwh-init--${VERSION}.tar.gz"
+
+
+# Собираем образ
 docker build --progress=plain -t "$IMAGE_TAG" .
+
+# Создаём каталог для архивов, если его ещё нет
+mkdir -p "$OUTPUT_DIR"
+
+# Сохраняем образ в tar.gz
+docker save "$IMAGE_TAG" | gzip > "$OUTPUT_FILE"
+
+echo "Образ $IMAGE_TAG сохранён в архив: $OUTPUT_FILE"
