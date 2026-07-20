75382faee6def71c67942954bbbcf47493b91efa
Alex <whites.russia@gmail.com>
2026-07-20 16:18:42 +0300

    [fix]: pxf в dwh-gp

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VERSION="0.12.4"
+VERSION="0.12.5"
 IMAGE_TAG="dwh-init:${VERSION}"
 
 OUTPUT_DIR="$HOME/images"
--- run-tests/af-pg-gpdb6-jks-gl-registry/dwh-start.sh
+++ run-tests/af-pg-gpdb6-jks-gl-registry/dwh-start.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-VERSION="0.12.4"
+VERSION="0.12.5"
 
 PROJECT_NAME="${PROJECT_NAME:-dwh-gp}"
 ARCHIVE_DIR="$HOME/images"
