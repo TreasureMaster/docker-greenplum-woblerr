adf79bb0c333a639c0dfd7cd75eff824020cc5c4
Alex <whites.russia@gmail.com>
2026-08-07 12:56:14 +0300

    [ref]: bump dwh-init

--- docker/dwh-init/build.sh
+++ docker/dwh-init/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VERSION="0.12.7"
+VERSION="0.12.8"
 IMAGE_TAG="dwh-init:${VERSION}"
 
 OUTPUT_DIR="$HOME/images"
--- run-tests/af-pg-gpdb6-jks-gl-registry/dwh-start.sh
+++ run-tests/af-pg-gpdb6-jks-gl-registry/dwh-start.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-VERSION="0.12.7"
+VERSION="0.12.8"
 
 PROJECT_NAME="${PROJECT_NAME:-dwh-gp}"
 ARCHIVE_DIR="$HOME/images"
